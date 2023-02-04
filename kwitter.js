3a9b21e40e6c139df5405602492277afea0b1222
Srihith <110789786+SrihithYarabati@users.noreply.github.com>
2023-02-04 19:46:55 +0000

    Add files via upload

---
+++ kwitter.js
@@ -0,0 +1,18 @@
+function addUser()
+{
+    user_name = document.getElementById("user_name").value;
+    localStorage.setItem("user_name", user_name);
+    window.location = "kwitter_room.html";
+}
+
+function adduser()
+      {
+room_names = document.getElementById("room_name").value;
+
+firebase.database().ref("/").child(room_name).update({
+      purpose : "adding room name"
+});
+localStorage.setItem("room_name", room_name);
+
+window.location = "kwitter_page.html";
+}
